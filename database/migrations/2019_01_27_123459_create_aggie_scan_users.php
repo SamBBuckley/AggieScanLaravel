fe76baa2008d9827d18acaa474b5cb9c6a76e118
SamBBuckley <SamBBuckley@tamu.edu>
2019-01-27 06:44:52 -0600

    Database

--- database/migrations/2019_01_27_123459_create_aggie_scan_users.php
+++ database/migrations/2019_01_27_123459_create_aggie_scan_users.php
@@ -17,6 +17,15 @@ class CreateAggieScanUsers extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('uin');
+            $table->string('cell_num');
+            $table->string('password');
+            $table->string('facebook');
+            $table->string('snap');
+            $table->string('twitter');
+            $table->string('major');
+            $table->string('year');
+            $table->string('password');
+
             $table->timestamps();
         });
     }
