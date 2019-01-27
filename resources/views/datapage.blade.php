02a1b70335f8f0f5f458b2b53627a96790434534
SamBBuckley <SamBBuckley@tamu.edu>
2019-01-27 09:27:36 -0600

    t

--- resources/views/datapage.blade.php
+++ resources/views/datapage.blade.php
@@ -10,7 +10,7 @@
 
         .qrCode {
             margin-top: 21%;
-            margin-left: 35%;
+            margin-left: 30%;
             position: absolute;
             width: 50%;
             height: auto;
