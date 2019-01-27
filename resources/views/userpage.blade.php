6e902316be0188cd05b2370f1c048fc9dbd4adfc
SamBBuckley <SamBBuckley@tamu.edu>
2019-01-27 10:17:30 -0600

    t

--- resources/views/userpage.blade.php
+++ resources/views/userpage.blade.php
@@ -1,3 +1,4 @@
+<<<<<<< current
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
@@ -34,7 +35,7 @@
         foreach ($users as $user) :
             if ($user->uin == $uin) {?>
                 <h1><?php echo $user->name ?></h1>
-                <img src="images/qr_{{$uin}}.png" alt="alt">
+                <img src="images/qr_{{echo $uin}}.png" alt="alt">
                 <p>Name: <?php echo $user->name ?></p>
                 <p>UIN: <?php echo $user->uin ?></p>
                 <p>Cell Number: <?php echo $user->cell_num ?></p>
@@ -49,3 +50,38 @@
 
     </body>
 </html>
+=======
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>QR Code</title>
+        <style media="screen">
+
+        </style>
+    </head>
+    <body>
+        <?php
+            $SnaggedUser = '';
+            $users = App\AggieScanUser::all();
+            foreach ($users as $user) {
+                if ($user->uin == $uin) {
+                    $SnaggedUser = $user;
+                }
+            }
+            endforeach;?>
+
+        <h1><?php echo $SnaggedUser->name ?></h1>
+        <img class='qrCode' src="images/qr_{{$SnaggedUser->uin}}.png" alt="qr_code">"
+        <p>Name: <?php echo $SnaggedUser->name ?></p>
+        <p>UIN: <?php echo $SnaggedUser->uin ?></p>
+        <p>Cell Number: <?php echo $SnaggedUser->cell_num ?></p>
+        <p>Facebook:<?php echo $SnaggedUser->facebook ?> </p>
+        <p>Snapchat: <?php echo $SnaggedUser->snap ?></p>
+        <p>Twitter: <?php echo $SnaggedUser->twitter ?></p>
+        <p>Major:<?php echo $SnaggedUser->major ?> </p>
+        <p>Graduation Year: <?php echo $SnaggedUser->year ?></p>
+
+    </body>
+</html>
+>>>>>>> before discard
