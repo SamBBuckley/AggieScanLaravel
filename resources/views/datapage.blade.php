ec7e11b70f0f6a3d21827cd498663c5bea7459d9
SamBBuckley <SamBBuckley@tamu.edu>
2019-01-27 08:52:20 -0600

    t

--- resources/views/datapage.blade.php
+++ resources/views/datapage.blade.php
@@ -16,9 +16,7 @@
     </head>
     <body>
         <ul>
-            <?php
-            $users = App\AggieScanUser::all();
-            foreach ($users as $user)?>
+        <?php $users = App\AggieScanUser::all(); foreach ($users as $user) : ?>
             <li><<?php $user->uin; ?></li>
         <?php endforeach; ?>
         </ul>
